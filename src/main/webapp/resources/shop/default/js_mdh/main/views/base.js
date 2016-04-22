a2ee4942ae812b80c68451c0d1b67fce408de28e
caorukang <1020617646@qq.com>
2016-04-22 19:30:26 +0800

    1： 导航

--- src/main/webapp/resources/shop/default/js_mdh/main/views/base.js
+++ src/main/webapp/resources/shop/default/js_mdh/main/views/base.js
@@ -1,6 +1,9 @@
 // 所有header，footer的js动态
 
 ;$(function() {
+	
+	 // 定时器
+  var isShowNav;
   if ($('.nav').offset()) {
     var y = $('.nav').offset().top;
 
@@ -20,14 +23,23 @@
 
   $('.nav>ul>li').hover(function() {
 	    var num=$('.innav ul li').length;
+	    var _this = this;
 	    if(num>0){
 	    	$('.innav ol li').eq(0).css('display','block').siblings().css('display','none');
 	    	$('.innav ul li').eq(0).addClass('current');
 	    }
-    $(this).find('.innav').slideDown(300);
-  }, function() {
-    $(this).find('.innav').slideUp(300);
-  });
+	    clearTimeout(isShowNav);
+	    isShowNav = setTimeout (function () {
+	       $(_this).find('.innav').slideDown(200);
+	    }, 100);
+	   
+	  }, function() {
+	    var _this = this;
+	    clearTimeout(isShowNav);
+	    isShowNav = setTimeout (function () {
+	      $(_this).find('.innav').slideUp(150);
+	    }, 100);
+	  });
   
   
 
